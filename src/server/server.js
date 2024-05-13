47fb1b65106a68bad5da39175b3ee058318a3cc4
AbstractMelon <129030233+AbstractMelon@users.noreply.github.com>
2024-05-13 17:38:27 -0600

    Did some update

--- src/server/server.js
+++ src/server/server.js
@@ -3,6 +3,11 @@ const app = express()
 const port = 3000
 const fs = require("fs")
 const path = require("path")
+const helmet = require('helmet');
+const morgan = require('morgan');
+
+app.use(morgan('dev'));
+app.use(helmet());
 
 var scripts = fs.readdirSync("./src/server/serverScripts").filter(e=>e.endsWith(".js"))
 scripts.forEach(script=>{
@@ -10,6 +15,17 @@ scripts.forEach(script=>{
     scr(app)
 })
 
+// 404
+app.use((req, res, next) => {
+  res.status(404).sendFile(path.join(__dirname, '../public/404.html'));
+});
+
+// Error handling
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).send('Something broke!');
+});
+
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+  console.log(`Azurite listening on port ${port}`)
 })
