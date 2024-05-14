793a31f76fcbf79babc9a21897032b31b1b8daca
AbstractMelon <129030233+AbstractMelon@users.noreply.github.com>
2024-05-13 20:22:30 -0600

    did the html

--- src/server/serverScripts/static.js
+++ src/server/serverScripts/static.js
@@ -1,10 +1,33 @@
-const express = require("express")
-const path = require("path")
+const express = require("express");
+const path = require("path");
 
 /**
  * 
  * @param {express.Application} app 
  */
-module.exports = (app)=>{
-    app.use(express.static(path.resolve("./src/public")))
-}
+module.exports = (app) => {
+    app.use(express.static(path.resolve("./src/public")));
+
+    // Route for /mod-manager
+    app.get("/mod-manager", (req, res) => {
+        res.sendFile(path.resolve("./src/public/html/modmanager.html"));
+    });
+
+
+    app.get("/downloads", (req, res) => {
+        res.sendFile(path.resolve("./src/public/html/downloads.html"));
+    });
+
+    app.get("/account", (req, res) => {
+        res.sendFile(path.resolve("./src/public/html/account/index.html"));
+    });
+
+    app.get("/games", (req, res) => {
+        res.sendFile(path.resolve("./src/public/html/games.html"));
+    });
+
+    app.get("/library", (req, res) => {
+        res.sendFile(path.resolve("./src/public/html/library.html"));
+    });
+
+};
