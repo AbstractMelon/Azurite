8873a15d97c8f3d7659a306d34370bba0654ce56
AbstractMelon <129030233+AbstractMelon@users.noreply.github.com>
2024-05-19 15:06:04 -0600

    First game and header updates

--- src/server/server.js
+++ src/server/server.js
@@ -1,19 +1,50 @@
-const express = require('express')
-const app = express()
-const port = 3000
-const fs = require("fs")
-const path = require("path")
+const express = require('express');
+const app = express();
+const port = 3000;
+const fs = require("fs");
+const path = require("path");
 const helmet = require('helmet');
 const morgan = require('morgan');
+
+const headerHTML = `
+<header>
+    <a href="/"><img src="/logo/azuritelogo.png" alt="Azurite Logo"></a>
+    <nav>
+        <a href="/">Home</a>
+        <a href="/games">Games</a>
+        <a href="/library">Library</a>
+        <a href="/mod-manager">Mod Manager</a>
+        <a href="/account">Account</a>
+    </nav>
+</header>
+`;
 
 app.use(morgan('dev'));
 app.use(helmet());
 
-var scripts = fs.readdirSync("./src/server/serverScripts").filter(e=>e.endsWith(".js"))
-scripts.forEach(script=>{
-    let scr = require("./serverScripts/"+script)
-    scr(app)
-})
+app.use((req, res, next) => {
+    res.injectHeader = (content) => {
+        return content.replace('<body>', `<body>${headerHTML}`);
+    };
+    next();
+});
+
+app.use((req, res, next) => {
+    const originalSend = res.send;
+    res.send = function (content) {
+        if (typeof content === 'string' && content.includes('<html')) {
+            content = res.injectHeader(content);
+        }
+        return originalSend.call(this, content);
+    };
+    next();
+});
+
+var scripts = fs.readdirSync("./src/server/serverScripts").filter(e => e.endsWith(".js"));
+scripts.forEach(script => {
+    let scr = require("./serverScripts/" + script);
+    scr(app);
+});
 
 // 404
 app.use((req, res, next) => {
@@ -28,4 +59,4 @@ app.use((err, req, res, next) => {
 
 app.listen(port, () => {
   console.log(`Azurite listening on port ${port}`)
-})
+});
