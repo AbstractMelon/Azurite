0d924f28dae73aaa363403f1e6e040717a4d13b6
AbstractMelon <129030233+AbstractMelon@users.noreply.github.com>
2024-05-20 16:16:48 -0600

    fix

--- src/server/serverScripts/createpages.js
+++ src/server/serverScripts/createpages.js
@@ -7,11 +7,13 @@ const path = require("path");
  * @param {string[]} gameNames
  */
 module.exports = (app, gameNames) => {
-    const GameNamesForPages = ["game1", "game2", "game3"];
+    if (!Array.isArray(gameNames)) {
+        throw new Error("gameNames must be an array");
+    }
 
     gameNames.forEach(gameName => {
         app.get(`/games/${gameName}`, (req, res) => {
-            res.sendFile(path.join(__dirname, 'public', 'games', `${gameName}.html`));
+            res.sendFile(path.join(__dirname, '../../public/games', `${gameName}.html`));
         });
     });
 };
