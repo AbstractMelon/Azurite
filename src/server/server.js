bb370d181be6b46870652c87346c5a9a6254439c
AbstractMelon <129030233+AbstractMelon@users.noreply.github.com>
2024-05-20 16:10:48 -0600

    Server and create opages

--- src/server/server.js
+++ src/server/server.js
@@ -6,19 +6,6 @@ const path = require("path");
 const helmet = require('helmet');
 const morgan = require('morgan');
 
-const headerHTML = `
-<header>
-    <a href="/"><img src="/logo/azuritelogo.png" alt="Azurite Logo"></a>
-    <nav>
-        <a href="/">Home</a>
-        <a href="/games">Games</a>
-        <a href="/library">Library</a>
-        <a href="/mod-manager">Mod Manager</a>
-        <a href="/account">Account</a>
-    </nav>
-</header>
-`;
-
 app.use(morgan('dev'));
 
 app.use(helmet({
@@ -29,24 +16,6 @@ app.use(helmet({
         },
     },
 }));
-
-app.use((req, res, next) => {
-    res.injectHeader = (content) => {
-        return content.replace('<body>', `<body>${headerHTML}`);
-    };
-    next();
-});
-
-app.use((req, res, next) => {
-    const originalSend = res.send;
-    res.send = function (content) {
-        if (typeof content === 'string' && content.includes('<html')) {
-            content = res.injectHeader(content);
-        }
-        return originalSend.call(this, content);
-    };
-    next();
-});
 
 var scripts = fs.readdirSync("./src/server/serverScripts").filter(e => e.endsWith(".js"));
 scripts.forEach(script => {
