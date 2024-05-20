2419fd943a93cf63cd6d87e3a6af4157bcf2429f
AbstractMelon <129030233+AbstractMelon@users.noreply.github.com>
2024-05-20 16:17:20 -0600

    Did stuff

--- src/server/serverScripts/createpages.js
+++ src/server/serverScripts/createpages.js
@@ -1,16 +1,13 @@
 const express = require("express");
 const path = require("path");
 
-/**
- * 
- * @param {express.Application} app 
- * @param {string[]} gameNames
- */
-module.exports = (app, gameNames) => {
-    if (!Array.isArray(gameNames)) {
-        throw new Error("gameNames must be an array");
-    }
 
+const gameNames = ["game1", "game2", "game3"];
+
+/**
+ * @param {express.Application} app
+ */
+module.exports = (app) => {
     gameNames.forEach(gameName => {
         app.get(`/games/${gameName}`, (req, res) => {
             res.sendFile(path.join(__dirname, '../../public/games', `${gameName}.html`));
