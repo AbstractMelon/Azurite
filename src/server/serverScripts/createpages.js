8fc6aad9f0424843ea54e05a7b26296903250e88
AbstractMelon <129030233+AbstractMelon@users.noreply.github.com>
2024-05-20 16:18:53 -0600

    one more fix

--- src/server/serverScripts/createpages.js
+++ src/server/serverScripts/createpages.js
@@ -2,7 +2,7 @@ const express = require("express");
 const path = require("path");
 
 
-const gameNames = ["game1", "game2", "game3"];
+const gameNames = ["bopl-battle", "game2", "game3"];
 
 /**
  * @param {express.Application} app
@@ -10,7 +10,7 @@ const gameNames = ["game1", "game2", "game3"];
 module.exports = (app) => {
     gameNames.forEach(gameName => {
         app.get(`/games/${gameName}`, (req, res) => {
-            res.sendFile(path.join(__dirname, '../../public/games', `${gameName}.html`));
+            res.sendFile(path.join(__dirname, '../../public/html/games', `${gameName}.html`));
         });
     });
 };
