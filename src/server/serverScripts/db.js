e33701ce0f686c318ce24583df02c5e6b69852dd
Crafterboy27 <117037081+minidogg@users.noreply.github.com>
2024-05-21 20:00:54 -0500

    aaaaaaaaaaaaaaaaaaaaa

--- src/server/serverScripts/db.js
+++ src/server/serverScripts/db.js
@@ -4,18 +4,28 @@ const fsUtils = require("../../utils/file.js")
 const dbPath = path.resolve("./src/database")
 
 
-function generateGame(name="Joe",description="Even better than Joe Classic!",id="joe",image="../../assets/images/games/joe.png"){
-    fsUtils.makeDir(path.join(dbPath,"games/"+id))
-    fsUtils.makeFile(path.join(dbPath,"games/"+id,"/manifest.json"),{
-        name,description,id,image
-    })
-}
+// function generateGame(name="Joe",description="Even better than Joe Classic!",id="joe",image="../../assets/images/games/joe.png"){
+//     fsUtils.makeDir(path.join(dbPath,"games/"+id))
+//     fsUtils.makeFile(path.join(dbPath,"games/"+id,"/manifest.json"),{
+//         name,description,id,image
+//     })
+// }
 
 function generateGame(name="Bopl Battle",description="Bopl Battle is a couch/online platform fighter game focused around battling your friends and combining unique and wild abilities together.",id="bopl-battle",image="../../assets/images/games/boplbattle.png"){
     fsUtils.makeDir(path.join(dbPath,"games/"+id))
     fsUtils.makeFile(path.join(dbPath,"games/"+id,"/manifest.json"),{
         name,description,id,image
     })
+}
+
+
+function getGames(){
+    var files = fs.readdirSync(path.join(dbPath,"/games"))
+    var games = files.map(e=>{
+        return JSON.parse(fs.readFileSync(path.join(dbPath,"/games/",e,"/manifest.json"),"utf-8"))
+    })
+
+    return games
 }
 
 /**
@@ -27,5 +37,18 @@ module.exports = (app) => {
     fsUtils.makeDir(path.join(dbPath,"users"))
     fsUtils.makeDir(path.join(dbPath,"games"))
 
-    generateGame()
+    generateGame("Bopl Battle",
+        `Bopl Battle is a couch/online platform fighter game focused around battling your friends and combining unique and wild abilities together.`
+    ,"bopl-battle","../../assets/images/games/boplbattle.png")
+    generateGame("Block Mechanic",
+        `Block Mechanic is a physics puzzle game, where you combine a selection of blocks of different types to build a vehicle.`
+    ,"block-mechanic","../../assets/images/games/blockmechanic.png")
+    generateGame("3Dash",
+        `3Dash, a three-dimensional autoscrolling platformer.`
+    ,"3dash","../../assets/images/games/3dash.png")
+    const games = getGames()
+
+    app.get("/api/v1/getGames",(req,res)=>{
+        res.json(games)
+    })
 };
