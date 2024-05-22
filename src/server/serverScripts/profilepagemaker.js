a894b844fd4be34291216ab287b9939002d2aefd
AbstractMelon <129030233+AbstractMelon@users.noreply.github.com>
2024-05-21 19:31:29 -0600

    Profiles, search bar, and a fix

---
+++ src/server/serverScripts/profilepagemaker.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const path = require("path");
+
+const userNames = ["azurite", "AbstractMelon", "UnluckyCrafter"];
+
+/**
+ * @param {express.Application} app
+ */
+module.exports = (app) => {
+    userNames.forEach(userName => {
+        app.get(`/user/${userName}`, (req, res) => {
+            res.sendFile(path.join(__dirname, '../../public/html/account/accounts/', `${userName}.html`));
+        });
+    });
+};
