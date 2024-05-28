fa48a729de5f1035985b73e59347b7ef21dec37f
AbstractMelon <129030233+AbstractMelon@users.noreply.github.com>
2024-05-27 20:01:34 -0600

    Formant and lint!

--- src/public/scripts/games.js
+++ src/public/scripts/games.js
@@ -1,38 +1,43 @@
 (async () => {
-    const gameListEl = document.getElementById("games-list-main");
+  const gameListEl = document.getElementById("games-list-main");
 
-    function addGame({name = "Joe", description = "Even better than Joe Classic!", id = "joe", image = "../../assets/images/games/joe.png"}) {
-        var gameCard = document.createElement("div");
-        gameCard.innerHTML = `<a href="/games/${id}">
+  function addGame({
+    name = "Joe",
+    description = "Even better than Joe Classic!",
+    id = "joe",
+    image = "../../assets/images/games/joe.png",
+  }) {
+    var gameCard = document.createElement("div");
+    gameCard.innerHTML = `<a href="/games/${id}">
             <img src="${image}" alt="${name}">
             <h2>${name}</h2>
             <p>${description}</p>
         </a>`;
-        gameCard.classList.add("game-card");
-        gameListEl.appendChild(gameCard);
-    }
+    gameCard.classList.add("game-card");
+    gameListEl.appendChild(gameCard);
+  }
 
-    const searchInput = document.getElementById("search-input");
-    const searchForm = document.getElementById("search-form");
+  const searchInput = document.getElementById("search-input");
+  const searchForm = document.getElementById("search-form");
 
-    let games = await (await fetch("/api/v1/getGames")).json();
+  let games = await (await fetch("/api/v1/getGames")).json();
 
-    function searchGames() {
-        const searchValue = searchInput.value.toLowerCase();
-        gameListEl.innerHTML = ""; 
-        Object.values(games).forEach(game => {
-            if (game.name.toLowerCase().includes(searchValue)) {
-                addGame(game);
-            }
-        });
-    }
-
-    searchGames();
-
-    searchInput.addEventListener("input", searchGames);
-
-    searchForm.addEventListener("submit", (e) => {
-        e.preventDefault();
-        searchGames();
+  function searchGames() {
+    const searchValue = searchInput.value.toLowerCase();
+    gameListEl.innerHTML = "";
+    Object.values(games).forEach((game) => {
+      if (game.name.toLowerCase().includes(searchValue)) {
+        addGame(game);
+      }
     });
+  }
+
+  searchGames();
+
+  searchInput.addEventListener("input", searchGames);
+
+  searchForm.addEventListener("submit", (e) => {
+    e.preventDefault();
+    searchGames();
+  });
 })();
--- src/server/serverScripts/static.js
+++ src/server/serverScripts/static.js
@@ -3,27 +3,29 @@ const express = require("express");
 const path = require("path");
 
 const quickFiles = {
-    "/mod-manager":path.resolve("./src/public/html/modmanager.html"),
-    "/downloads":path.resolve("./src/public/html/downloads/download.html"),
-    "/account":path.resolve("./src/public/html/account/index.html"),
-    "/games":path.resolve("./src/public/html/games/games.html"),
-    "/library":path.resolve("./src/public/html/library.html"),
-    "/testing":path.resolve("./src/public/testing.html"),
-    "/login":path.resolve("./src/public/html/account/login.html"),
-    "/logo/azuritelogo.png":path.resolve("./src/public/assets/images/azuritelogo.png")
-}
+  "/mod-manager": path.resolve("./src/public/html/modmanager.html"),
+  "/downloads": path.resolve("./src/public/html/downloads/download.html"),
+  "/account": path.resolve("./src/public/html/account/index.html"),
+  "/games": path.resolve("./src/public/html/games/games.html"),
+  "/library": path.resolve("./src/public/html/library.html"),
+  "/testing": path.resolve("./src/public/testing.html"),
+  "/login": path.resolve("./src/public/html/account/login.html"),
+  "/logo/azuritelogo.png": path.resolve(
+    "./src/public/assets/images/azuritelogo.png",
+  ),
+};
 /**
- * 
- * @param {express.Application} app 
+ *
+ * @param {express.Application} app
  */
 module.exports = (app) => {
-    app.use(express.static(path.resolve("./src/public")));
+  app.use(express.static(path.resolve("./src/public")));
 
-    app.use((req,res,next)=>{
-        if(quickFiles[req.path]){
-            res.sendFile(quickFiles[req.path])
-            return
-        }
-        next()
-    })
+  app.use((req, res, next) => {
+    if (quickFiles[req.path]) {
+      res.sendFile(quickFiles[req.path]);
+      return;
+    }
+    next();
+  });
 };
