620cb95377508f67fa3fbd950ac7be3b8eb7267b
AbstractMelon <129030233+AbstractMelon@users.noreply.github.com>
2024-08-13 13:17:02 -0600

    Fix file tracking

--- .next/static/development/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST = {__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/":["static\u002Fchunks\u002Fpages\u002Findex.js"],"/_error":["static\u002Fchunks\u002Fpages\u002F_error.js"],"/games":["static\u002Fchunks\u002Fpages\u002Fgames.js"],sortedPages:["\u002F","\u002F_app","\u002F_error","\u002Fgames"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
